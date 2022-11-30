7e946f3c352b59f4ea03132dcdb6459be1082724
Ben Purinton <ben.purinton@gmail.com>
2022-11-30 04:52:36 +0000

    some more api progress

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -1,9 +1,20 @@
 class ApisController < ApplicationController
   def map2coord_form
-    render({ :template => "api_html/map2coord_form.html.erb"})
+    render({ :template => "api_html/map2coord_form.html.erb" })
   end
+
   def map2coord
     @user_street_address = params.fetch("user_street_address")
-    render({ :template => "api_html/map2coord.html.erb"})
+    @url = "https://maps.googleapis.com/maps/api/geocode/json?address=5807+S+Woodlawn+Ave&key=" + ENV.fetch("GMAPS_KEY")
+    raw_data = open(@url).read
+    parsed_data = JSON.parse(raw_data)
+    results_array = parsed_data.fetch("results")
+    first_result = results_array.at(0)
+    geometry_hash = first_result.fetch("geometry")
+    location_hash = geometry_hash.fetch("location")
+    @latitude = location_hash.fetch("lat")
+    @longitude = location_hash.fetch("lng")
+
+    render({ :template => "api_html/map2coord.html.erb" })
   end
 end
